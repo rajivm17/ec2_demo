3aaed3104eafff270af73acd4b74332619dd37e4
rajivm17 <82876822+rajivm17@users.noreply.github.com>
2021-08-27 00:11:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
     }
   }
-  required_version = ">= 0.14"
 
   backend "remote" {
     organization = "rajiv-terraformcloud"
