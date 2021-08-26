05f31ba8f950866ceacbd2405fc4e0338eb6c323
rajivm17 <82876822+rajivm17@users.noreply.github.com>
2021-08-26 23:51:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
     }
   }
-  required_version = ">= 0.14"
 
   backend "remote" {
     organization = "rajiv-terraformcloud"
@@ -22,7 +19,7 @@ terraform {
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
 
